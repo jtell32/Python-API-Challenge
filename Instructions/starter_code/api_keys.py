ca2424eba9a3f3ec8ed1530f057fb371a23ab121
jtell32 <101846606+jtell32@users.noreply.github.com>
2022-05-05 21:33:37 -0500

    added api_keys.py to ignore

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6c2fe231ef6a578711b7ecce978c381c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAmpqC1tSIOi0Ui8RM7C6I09hQjko5auJk"
