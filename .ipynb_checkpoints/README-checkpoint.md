65ae0efd4614c8711880743854669bc4124ccaa3
jtell32 <101846606+jtell32@users.noreply.github.com>
2022-05-07 15:01:57 -0500

    updating weatherpy.ipynb

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,3 @@
+# Python-API-Challenge
+hello world
+
---
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "6c2fe231ef6a578711b7ecce978c381c"
+
+# Google API Key
+g_key = "AIzaSyAmpqC1tSIOi0Ui8RM7C6I09hQjko5auJk"
